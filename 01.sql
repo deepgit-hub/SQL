1d061e9980ce34a2cf4e11299644100004c83ce0
Deepak <darkdeepak001@gmail.com>
2026-06-18 21:44:56 +0530

    Update and rename d to 01.sql

---
+++ 01.sql
@@ -0,0 +1,21 @@
+SQL Question 1
+
+Create a table called:
+
+Patient
+
+with these columns:
+
+Column	Type
+Name	VARCHAR(50)
+Age	INT
+Disease	VARCHAR(50)
+
+After creating the table:
+
+Insert the following records:
+
+Name	Age	Disease
+Ravi	45	Diabetes
+Priya	30	Fever
+Karthik	25	Asthma
