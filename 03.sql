e97d335c54c1555f53e3394c6b9b5223b2410a79
Deepak <darkdeepak001@gmail.com>
2026-06-19 06:46:19 +0530

    Update patient's disease to 'Typhoid' for Kathir

--- 03.sql
+++ 03.sql
@@ -27,3 +27,17 @@ SELECT * FROM Patient;
 | Dea     |   25 | Throught_pain |
 +---------+------+---------------+
 UPDATE Patient SET Disease='Typhoid' WHERE Name='Kathir';
+SELECT * FROM Patient;
++---------+------+---------------+
+| Name    | Age  | Disease       |
++---------+------+---------------+
+| Chandra |   56 | Fever         |
+| Chandra |   56 | Fever         |
+| Seker   |   26 | High Fever    |
+| Es      |   25 | Astuma        |
+| Kathir  |   14 | Typhoid       |
+| Deepa   |   45 | Throught_pain |
+| As      |   65 | Astuma        |
+| thir    |   34 | Infection     |
+| Dea     |   25 | Throught_pain |
++---------+------+---------------+
