37cbf33c6b50a540b334390f2a1e74756e85f0ed
Deepak <darkdeepak001@gmail.com>
2026-06-19 06:49:53 +0530

    Add deletion preview for patient records

--- 04.sql
+++ 04.sql
@@ -14,3 +14,17 @@ Verify that only Es is removed.
 DELETE
 WHERE
 SELECT *
+Before Deletion 
++---------+------+---------------+
+| Name    | Age  | Disease       |
++---------+------+---------------+
+| Chandra |   56 | Fever         |
+| Chandra |   56 | Fever         |
+| Seker   |   26 | High Fever    |
+| Es      |   25 | Astuma        |
+| Kathir  |   14 | Typhoid       |
+| Deepa   |   45 | Throught_pain |
+| As      |   65 | Astuma        |
+| thir    |   34 | Infection     |
+| Dea     |   25 | Throught_pain |
++---------+------+---------------+
