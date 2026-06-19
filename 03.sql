7eafcde185b74eba1ebfdb902f4faee1c1f70f1a
Deepak <darkdeepak001@gmail.com>
2026-06-19 06:45:10 +0530

    Add query to display Patient table records
    Added a query to display all records from the Patient table after an update.

--- 03.sql
+++ 03.sql
@@ -12,3 +12,17 @@ Typhoid
 
 After updating, display the entire table to verify the change
 -----------------------------------------------------QUERY--------------------------------------------------------------------
+SELECT * FROM Patient;
++---------+------+---------------+
+| Name    | Age  | Disease       |
++---------+------+---------------+
+| Chandra |   56 | Fever         |
+| Chandra |   56 | Fever         |
+| Seker   |   26 | High Fever    |
+| Es      |   25 | Astuma        |
+| Kathir  |   14 | Infection     |
+| Deepa   |   45 | Throught_pain |
+| As      |   65 | Astuma        |
+| thir    |   34 | Infection     |
+| Dea     |   25 | Throught_pain |
++---------+------+---------------+
