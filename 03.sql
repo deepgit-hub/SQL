7b048d24685c3bf2cc210e8983bc2c108964341c
Deepak <darkdeepak001@gmail.com>
2026-06-19 06:44:03 +0530

    Change disease of patient Kathir to Typhoid
    Update the disease of patient Kathir from Infection to Typhoid and display the updated table.

---
+++ 03.sql
@@ -0,0 +1,13 @@
+SQL Question 3
+
+Using the same Patient table:
+
+👉 Change the disease of Kathir from:
+
+Infection
+
+to
+
+Typhoid
+
+After updating, display the entire table to verify the change
