2f2cf893c1369c000a3764b7c28cc8d5d43769a4
Deepak <darkdeepak001@gmail.com>
2026-06-21 20:40:52 +0530

    Rename Student table to Student_Details

--- 03.sql
+++ 03.sql
@@ -1,2 +1,4 @@
 Rename the Student table as Student_Details.
 -----------------------------------------------------QUERY----------------------------------------------------------------------
+ALTER TABLE Student
+    -> RENAME TO Student_Details;
