333dd3e98562aa15ad03fa244034a0e0baa6c29d
Deepak <darkdeepak001@gmail.com>
2026-06-21 20:37:29 +0530

    Add Email column to Student table

--- 02.sql
+++ 02.sql
@@ -1,2 +1,4 @@
 Add an Email column to the Student table.
 ---------------------------------------------------QUERY-----------------------------------------------------------------------
+ ALTER TABLE Student
+    -> ADD Email INT;
