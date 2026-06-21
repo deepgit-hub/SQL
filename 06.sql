da4cf004a839c952133aa4ac78f373b770fddc85
Deepak <darkdeepak001@gmail.com>
2026-06-21 21:09:36 +0530

    Create Student_Details table with constraints

--- 06.sql
+++ 06.sql
@@ -1,3 +1,4 @@
 Primary Key on StudentID
 NOT NULL on StudentName
 -----------------------------------------------------QUERY----------------------------------------------------------------------
+CREATE TABLE Student_Details(StudentId INT PRIMARY KEY,StudentName VARCHAR(20) NOT NULL , Department VARCHAR(20), Age INT);
